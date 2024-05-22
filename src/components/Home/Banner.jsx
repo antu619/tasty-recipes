4d589af800b790efbe1961e44642e5deb1534a21
Antu Chandra Das <antu.das.619@gmail.com>
2024-05-22 14:07:04 +0600

    Added banner section

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,9 +1,16 @@
 
 const Banner = () => {
     return (
-        <div>
-            Banner
-        </div>
+        <div className="hero h-[720px]" style={{backgroundImage: 'url(https://i.ibb.co/8NcsR50/tasty-recipes-cover.png)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-2xl">
+      <h1 className=" text-6xl font-bold text-white">Welcome To Tasty<br /> Recipes</h1>
+      <p className="my-10">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-outline text-white border-white hover:bg-white hover:text-black w-40">Order Now</button>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 px-0">
+        <div className="navbar bg-[#eb5757] text-white px-10">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
